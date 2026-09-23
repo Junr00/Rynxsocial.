b31cfe6d0778f78893a0f3314bc52e55378cda4b
Junr00 <vickkymicheal4real@gmail.com>
2026-09-23 19:54:41 +0100

    Update MainActivity.kt

--- app/src/main/res/values/java/com/rynx/social/MainActivity.kt
+++ app/src/main/res/values/java/com/rynx/social/MainActivity.kt
@@ -1,25 +1,22 @@
 package com.rynx.social
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import android.graphics.Color
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            MaterialTheme {
-                RynxScreen()
-            }
-        }
-    }
-}
+        val text = TextView(this)
 
-@Composable
-fun RynxScreen() {
-    Text("Rynx Social 🚀")
+        text.text = "Rynx Social 🚀"
+        text.textSize = 28f
+        text.setTextColor(Color.BLACK)
+        text.setPadding(40, 40, 40, 40)
+
+        setContentView(text)
+    }
 }
